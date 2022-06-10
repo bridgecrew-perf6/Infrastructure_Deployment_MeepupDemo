fccc406a34f797b1040084ea1e3b331a1d6b2cbb
nugowe <nosa.ugowe@yahoo.com>
2022-06-10 07:22:00 -0500

    made changes to github actions yaml

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,13 @@
+
 terraform {
+  required_version = ">=0.12.0"
+  required_providers {
+    aws = ">=3.0.0"
+  }
   backend "s3" {
     region  = "us-east-2"
-    bucket  = "demo-meetup-infra-state-files"
+    profile = "default"
     key     = "demo-meetup-infra-state-files"
-    encrypt = true #AES-256encryption
+    bucket  = "demo-meetup-infra-state-files"
   }
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,11 @@
 # provider.tf
-
-# Specify the provider and access details
-terraform {
-  required_version = ">= 0.13"
+provider "aws" {
+  profile                 = var.profile
+  shared_credentials_file = "~/.aws/credentials"
+  region                  = us-east-2
+  
 }
 
 provider "aws" {
-  region  = "us-east-2"
-  profile = "default"
-  version = "~> 2.66.0"  # version changed
+  region = "us-east-2"
 }
